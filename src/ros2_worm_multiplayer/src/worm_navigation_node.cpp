3d1fea086a2fbd300903420340e9a1ceac59d89e
Joshua Peller <jopel@jopel.de>
2023-11-09 19:40:28 +0100

    First tests were succesfull

--- src/ros2_worm_multiplayer/src/worm_navigation_node.cpp
+++ src/ros2_worm_multiplayer/src/worm_navigation_node.cpp
@@ -1,7 +1,10 @@
 #include <cstdio>
+#include <curses.h>
+#include <termios.h>
 #include <cinttypes>
 #include "rclcpp/rclcpp.hpp"
 #include "ros2_worm_multiplayer/msg/direction.hpp"
+#include "worm_constants.hpp"
 
 /**
  * @brief
@@ -13,6 +16,8 @@ class Navigation : public rclcpp::Node
 		void inputLoop();
 
 	private:
+		void initializeCursesApplication();
+
 		/* Direction */
 		int8_t dx_, dy_;
 
@@ -29,11 +34,14 @@ class Navigation : public rclcpp::Node
 Navigation::Navigation()
 : Node("navigation_node"), dx_(0), dy_(0)
 {
+	/* NCurses Init */
+	this->initializeCursesApplication();
+
 	/* rclcpP::QoS(10) provides default QoS profile with history depth of 10 */
 	this->direction_pub_ = this->create_publisher<ros2_worm_multiplayer::msg::Direction>("PlayerInput", rclcpp::QoS(10));
 
 	/* Create a timer to check for keyboard input every 100ms */
-	this->timer_ = create_wall_timer(std::chrono::milliseconds(100), std::bind(&Navigation::inputLoop, this));
+	this->timer_ = create_wall_timer(WormConstants::TICK_TIME, std::bind(&Navigation::inputLoop, this));
 }
 
 /**
@@ -41,7 +49,74 @@ Navigation::Navigation()
 */
 void Navigation::inputLoop()
 {
-	// TODO
+	/* Check if a key is pressed */
+	int key;
+	bool dirty = false;
+
+	if ((key = getch()) != ERR)
+	{
+		/* msg to store values */
+		ros2_worm_multiplayer::msg::Direction wormDirection;
+
+	  switch (key)
+	  {
+	  case 'q':
+	  	// todo
+			dirty = true;
+	  	break;
+	
+	  case KEY_UP :
+			wormDirection.dx = 0;
+			wormDirection.dy = -1;
+			dirty = true;
+	  	break;
+
+	  case KEY_DOWN :
+			wormDirection.dx = 0;
+			wormDirection.dy = 1;
+			dirty = true;
+	  	break;
+
+	  case KEY_LEFT :
+			wormDirection.dx = -1;
+			wormDirection.dy = 0;
+			dirty = true;
+	  	break;
+
+	  case KEY_RIGHT :
+			wormDirection.dx = 1;
+			wormDirection.dy = 0;
+			dirty = true;
+	  	break;
+	  }
+
+		if (dirty == true)
+		{
+			this->direction_pub_->publish(wormDirection);
+			dirty = false;
+		}
+	}
+	return;
+}
+
+/**
+ * 
+*/
+void Navigation::initializeCursesApplication()
+{
+	initscr(); // Initialize the curses screen
+
+	// Note:
+	// The call to initscr() defines various global variables of the curses framework.
+	// stdscr, LINES, COLS, TRUE, FALSE
+
+	noecho();  // Characters typed ar not echoed
+	cbreak();  // No buffering of stdin
+	nonl();    // Do not translate 'return key' on keyboard to newline character
+	keypad(stdscr, TRUE); // Enable the keypad
+	curs_set(0);          // Make cursor invisible
+	// Begin in non-single-step mode (getch will not block)
+	nodelay(stdscr, TRUE);  // make getch to be a non-blocking call
 }
 
 /* */
