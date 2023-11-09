7c6b8258a053de84490555e2d78d1cc48d753369
Joshua Peller <jopel@jopel.de>
2023-11-09 18:40:09 +0100

    modified

---
+++ src/ros2_worm_multiplayer/include/worm_constants.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef _WORM_CONSTANTS_HPP
+#define _WORM_CONSTANTS_HPP
+
+#include <chrono>
+
+namespace WormConstants 
+{
+constexpr std::chrono::milliseconds TICK_TIME{100};
+}
+#endif
